da2c50c282597c631757332f17c5b85afb1ddfd1
Trevor Davis <davist11@gmail.com>
2011-04-19 21:54:04 -0400

    heelp was breaking some JS on publish pages

--- third_party/heelp/acc.heelp.php
+++ third_party/heelp/acc.heelp.php
@@ -5,7 +5,7 @@ class Heelp_acc {
 	var $name		= 'Heelp!';
 	var $id			= 'heelp';
 	var $version		= '1.0';
-	var $description	= 'Provide channel specific help on the publish pages.';
+	var $description	= 'Provide instructions to clients on the channel publish pages.';
 	var $sections		= array();
 
 	/**
@@ -29,7 +29,7 @@ class Heelp_acc {
 	  
 	  if($_GET['C'] == 'content_publish')
 	  {
-	    $channel_id = $_GET['channel_id'];
+	    $channel_id = $this->EE->input->get('channel_id');
   	  if( isset($channel_id) && @file_exists(APPPATH . 'third_party/heelp/views/channel_' . $channel_id . '.php') )
   	  {
   	    $this->sections['Helpful Hints'] = $this->EE->load->view('channel_' . $channel_id, NULL, TRUE);  
